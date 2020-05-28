d92a94e0b8e390e35ebfe2187b4f2b0ea78874bc
sbhalla98 <sonalibhalla98@gmail.com>
2020-05-28 10:29:03 +0530

    added view to edit a contact

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('addcontact', { title: 'MyPhoneBook' });
+router.get('/get', function(req, res, next) {
+  res.render('index', { title: 'MyPhoneBook' });
 });
 
 module.exports = router;
